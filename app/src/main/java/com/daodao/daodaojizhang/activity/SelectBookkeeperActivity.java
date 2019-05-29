e2ed32ac988b0bf1d8dbd3770a5329081c1a3b0b
CHINA\qxx0101 <Dongyan.Xie@partner.bmw.com>
2019-05-29 15:56:21 +0800

    Initial commit

---
+++ app/src/main/java/com/daodao/daodaojizhang/activity/SelectBookkeeperActivity.java
@@ -0,0 +1,15 @@
+package com.daodao.daodaojizhang.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.daodao.daodaojizhang.R;
+
+public class SelectBookkeeperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_bookkeeper);
+    }
+}
